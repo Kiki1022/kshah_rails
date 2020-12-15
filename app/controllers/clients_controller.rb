814cea7137260f941fe4f1b591d2c0a9b67eb9c6
kinnari <kinnari1022@yahoo.com>
2020-12-14 22:01:44 -0500

    update clients controller and client view forms.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,4 +8,33 @@ class ClientsController < ApplicationController
         @client = Client.find(params[:id])
     end
 
+    def new
+        @client = Client.new
+    end
+
+    def create
+        @client = Client.create(client_params)
+
+    end
+
+    def edit
+        @client = Client.find_by(id: params[:id])
+    end
+
+    def update
+        @client.update(client_params)
+        @client.save
+    end
+
+    def destroy
+
+    end
+
+    private
+
+        def client_params
+            params.require(:client).permit(:name, :notes)
+        end
+
+
 end

cd9e3edc33dd0757bf7cc8b6c425ca5d2ec25fd3
kinnari <kinnari1022@yahoo.com>
2020-12-14 23:13:34 -0500

    first commit

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,6 @@ class Client < ApplicationRecord
     has_many :appointments
     has_many :stylists, through: :appointments
    
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
 
 end
